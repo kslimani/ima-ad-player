fb3483d94837f2646a259d9506c45d5ce5fb180f
Karim Slimani <karim@playmedia.fr>
2018-12-11 17:29:44 +0100

    chore: rebuild dist

--- dist/ima-ad-player.js
+++ dist/ima-ad-player.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["GoogleImaVideo"] = factory();
+		exports["ImaAdPlayer"] = factory();
 	else
-		root["GoogleImaVideo"] = factory();
+		root["ImaAdPlayer"] = factory();
 })(window, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
