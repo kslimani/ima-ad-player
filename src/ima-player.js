61ccfbc8f36c453d51384d4a5e745ff221484810
Karim Slimani <karim@playmedia.fr>
2019-05-31 11:36:56 +0200

    fix: ensure ad is requested before start

--- src/ima-player.js
+++ src/ima-player.js
@@ -7,6 +7,7 @@ export default class ImaPlayer {
   constructor(options) {
     this._configure(options)
     this._evt = new Observable()
+    this._adRequesting = false
     this._adRequested = false
 
     // https://developers.google.com/interactive-media-ads/docs/sdks/html5/v3/apis#ima.ImaSdkSettings.setVpaidMode
@@ -178,7 +179,13 @@ export default class ImaPlayer {
   _requestAd(options) {
     this._end = false
 
-    // Check if ad pre-requested
+    // Check if ad request is pending
+    if (this._adRequesting) {
+      // Ad will autostart if play method called
+      return
+    }
+
+    // Check if ad already requested (pre-request)
     if (this._adRequested) {
       // Start ad only if play method called
       if (this._adPlayIntent) {
@@ -188,7 +195,7 @@ export default class ImaPlayer {
       return
     }
 
-    this._adRequested = true
+    this._adRequesting = true
 
     if (! this._adsLoader) {
       this._makeAdsLoader()
@@ -323,6 +330,10 @@ export default class ImaPlayer {
     this._adsManager = adsManagerLoadedEvent.getAdsManager(this._o.video, adsRenderingSettings)
     this._bindAdsManagerEvents()
 
+    // Ad is ready to be played
+    this._adRequesting = false
+    this._adRequested = true
+
     if (this._adPlayIntent) {
       this._playAd()
     }
@@ -384,6 +395,7 @@ export default class ImaPlayer {
 
     this._end = true
     this._adPlayIntent = false
+    this._adRequesting = false
     this._adRequested = false
     this._resetMaxDurationTimer()
     this._dispatch('ad_end')
