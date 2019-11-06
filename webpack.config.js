2b0f1cdf930256b8b7be7a682318a016b364bd0d
Karim Slimani <karim@playmedia.fr>
2019-11-06 12:42:57 +0100

    chore: update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
 // Webpack 4 configuration
 const path = require('path')
 const webpack = require('webpack')
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
 const NotifierPlugin = require('webpack-build-notifier')
 
-var minify, outputFile, plugins = []
+var name = 'ima-ad-player'
+var outputFile, plugins = [], optimization = {}
 
 if (process.env.npm_lifecycle_event === 'dist') {
-  outputFile = 'ima-ad-player.min.js'
-  minify = true
+  outputFile = name + '.min.js'
+  optimization.minimizer = [
+    new UglifyJsPlugin({
+      cache: true,
+      parallel: true,
+      uglifyOptions: {
+        output: {
+          comments: false,
+        },
+      }
+    }),
+  ]
 } else {
-  outputFile = 'ima-ad-player.js'
-  minify = false // Do not minify ES5 compiled module
+  outputFile = name + '.js'
+  optimization.minimize = false
 }
 
 plugins.push(new NotifierPlugin({
@@ -19,15 +31,13 @@ plugins.push(new NotifierPlugin({
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
-  optimization: {
-    minimize: minify,
-  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: outputFile,
     library: 'ImaAdPlayer',
     libraryTarget: 'umd',
   },
+  optimization: optimization,
   module: {
     rules: [
       {
