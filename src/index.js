d3b6a001562fe800d8a2b80f30612bc9fd82be64
Karim Slimani <karim@playmedia.fr>
2021-01-29 20:20:17 +0100

    feat: add set of constants for VPAID modes.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ImaPlayer from './ima-player'
 import {makeNum} from './utils'
 
 // IMA ad player factory
-export default function (options, cb) {
+function factory(options, cb) {
   imaLoader((success) => {
     if (! success) {
       return cb(null, new Error('Failed to load IMA SDK'))
@@ -12,3 +12,8 @@ export default function (options, cb) {
     cb(new ImaPlayer(options), null)
   }, makeNum(options.timeout, 6000), !!options.debug)
 }
+
+// Make VPAID modes available before player instanciation (read only)
+factory.vpaidMode = Object.freeze(ImaPlayer.vpaidMode)
+
+export { factory as default }
